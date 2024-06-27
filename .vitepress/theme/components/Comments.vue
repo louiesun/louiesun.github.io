a5118df3360e4293384ef0bb2b2fdc25269faeec
louiesun <95512329+louiesun@users.noreply.github.com>
2024-06-27 21:09:57 +0800

    update comment (failed)

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -1,39 +1,52 @@
 <template>
-  <div id="gitalk-container"></div>
+  <p>Here should be the comments section. From here</p>
+  <Giscus
+    id="comments"
+    repo="louiesun/louiesun.github.io"
+    repoId="R_kgDOMOvc_A"
+    category="Announcements"
+    categoryId="DIC_kwDOMOvc_M4CgbNX"
+    mapping="specific"
+    term={{ Md5.hashStr(location.pathname) }}
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="preferred_color_scheme"
+    lang="zh-CN"
+  />
+  <p>To here, But it May not Appear because of the Giscus API is not working or I use the wrong code. </p>
+  <details>
+    <summary>This is the code importing Giscus. </summary> 
+    &lt;Giscus <br/>
+    id=&quot;comments&quot;<br/>
+    repo=&quot;louiesun/louiesun.github.io&quot;<br/>
+    repoId=&quot;R_kgDOMOvc_A&quot;<br/>
+    category=&quot;Announcements&quot;<br/>
+    categoryId=&quot;DIC_kwDOMOvc_M4CgbNX&quot;<br/>
+    mapping=&quot;specific&quot;<br/>
+    term=\{\{ Md5.hashStr(location.pathname) \}\}<br/>
+    reactionsEnabled=&quot;1&quot;<br/>
+    emitMetadata=&quot;0&quot;<br/>
+    inputPosition=&quot;top&quot;<br/>
+    theme=&quot;preferred_color_scheme&quot;<br/>
+    lang=&quot;zh-CN&quot;<br/>
+    /&gt;<br/>
+    <br/>
+    and in the script part:<br/>
+    <br/>
+    import {Md5} from 'ts-md5';<br/>
+    import { onContentUpdated, useRouter } from &quot;vitepress&quot;;<br/>
+    import Giscus from '@giscus/vue';<br/>
+  </details>
 </template>
 <script lang="ts" setup>
-import "gitalk/dist/gitalk.css";
-import Gitalk from "gitalk";
 import {Md5} from 'ts-md5';
 import { onContentUpdated, useRouter } from "vitepress";
-
-// const { route, go } = useRouter();
-function deleteChild(element: HTMLDivElement | null) {
-  let child = element?.lastElementChild;
-  while (child) {
-    element?.removeChild(child);
-    child = element?.lastElementChild;
-  }
-}
+import Giscus from '@giscus/vue';
 
 onContentUpdated(() => {
-  // reset gittalk element for update
-  const element = document.querySelector("#gitalk-container");
-  if (!element) {
-    return;
-  }
-  deleteChild(element);
-  const gitalk = new Gitalk({
-    clientID: "Ov23ctu9tK2Jjv9Clc81",
-    clientSecret: "110c9f51727015b925d084876ecfbda28a4b0074",
-    repo: "BlogComments",
-    owner: "louiesun",
-    admin: ["louiesun"],
-    id: Md5.hashStr(location.href), // Ensure uniqueness and length less than 50
-    language: "zh-CN",
-    distractionFreeMode: true, // Facebook-like distraction free mode
-  });
-  gitalk.render("gitalk-container");
+  
+
 });
 </script>
 <style scoped></style>
