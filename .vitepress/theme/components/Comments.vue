a87994a2a0d9f39a6c61419613ff48b68bb33ba5
louiesun <95512329+louiesun@users.noreply.github.com>
2024-06-28 15:10:37 +0800

    Comments Are on!
    Midi Player is developing.

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -1,19 +1,32 @@
-<template>
-  <p>The comments will be ok. </p>
-  <!--
-  <Giscus id="comments" repo="louiesun/louiesun.github.io" repoId="R_kgDOMOvc_A" category="Announcements"
-    categoryId="DIC_kwDOMOvc_M4CgbNX" mapping="pathname" reactionsEnabled="1" emitMetadata="0"
-    inputPosition="top" theme="preferred_color_scheme" lang="zh-CN" />
-    -->
-</template>
-<script lang="ts" setup>
+<script setup lang="ts">
 import { Md5 } from 'ts-md5';
 import { onContentUpdated, useRouter } from "vitepress";
 import Giscus from '@giscus/vue';
+import { ref } from 'vue';
 
-
-
+let pathname = ref(location.pathname);
+let MD5pathname = ref(Md5.hashStr(pathname));
 onContentUpdated(() => {
+  pathname.value = location.pathname;
+  MD5pathname.value = Md5.hashStr(pathname.value);
 });
+
 </script>
+<template>
+  <Giscus id="comments"
+    repo="louiesun/louiesun.github.io"
+    repo-id="R_kgDOMOvc_A"
+    category="General"
+    category-id="DIC_kwDOMOvc_M4CgbNY"
+    mapping="specific"
+    :term="MD5pathname"
+    reactions-enabled="1"
+    emit-metadata="0"
+    input-position="top"
+    theme="preferred_color_scheme"
+    lang="zh-CN"
+    crossorigin="anonymous"
+    :key="MD5pathname"
+    async />
+</template>
 <style scoped></style>
---
+++ posts/Poetry/MidiPlayer.vue
@@ -0,0 +1,64 @@
+<template>
+    <h2>This is a MidiPlayer</h2>
+    <p>MidiUrl: {{ MidiUrl }}</p>
+    <p>SongTitle: {{ SongTitle }}</p>
+    <div id="player"></div>
+</template>
+<script lang="ts">
+/*
+import { JZZ } from 'jzz';
+import { SMF } from 'jzz-midi-smf';
+import { Player } from 'jzz-gui-player';
+*/
+import { ref } from 'vue';
+
+export default {
+    setup(props) {
+        /*SMF(JZZ);
+        Player(JZZ);*/
+        const MidiUrl = ref(props.MidiUrl);
+        const SongTitle = ref(props.SongTitle);
+        var player;
+        function relative2absolute(url: string, base: string) {
+            if (!base) {
+                base = location.protocol + location.host;
+            }
+            return new URL(url, base).href;
+        }
+        function InitPlayer() {
+            console.log(relative2absolute(MidiUrl.value,location.href))
+            //fetch(MidiUrl.value)
+            /*fetch(relative2absolute(MidiUrl.value,location.href))
+                .then(response => response.arrayBuffer())
+                .then(arrayBuffer => {
+                    console.log(arrayBuffer);
+                    player = new JZZ.gui.Player('player');
+                    var smf = new JZZ.MIDI.SMF(arrayBuffer);
+                    player.load(smf); // MIDI 1.0, or
+                    console.log(player);
+                    // player.load(new JZZ.MIDI.Clip(data)); // MIDI 2.0
+                    console.log("WebMidi enabled!Trying to play " + MidiUrl.value + ".");
+                })
+                .catch(error => console.error(error));
+                */
+        }
+        InitPlayer();
+
+
+        return {
+            MidiUrl,
+            SongTitle,
+        };
+    },
+    props: {
+        MidiUrl: {
+            type: String,
+            required: true
+        },
+        SongTitle: {
+            type: String,
+            default: 'Song.mid'
+        },
+    }
+}
+</script>
